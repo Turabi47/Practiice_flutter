feddec9a09a60b4a5e219489f324d75464c0be05
curious <akturabi47@gmail.com>
2026-02-22 07:55:10 +0500

    dart basics

---
+++ get input.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+void main() {
+  // stdout.write("Enter your name\n");
+  // String name = stdin.readLineSync().toString();
+  // stdout.write("Hello, $name");
+  //
+  // stdout.write("Enter a number\n");
+  // int no1 = int.parse(stdin.readLineSync().toString());
+  // int sqr = no1 * no1;
+  // stdout.write("Square of $no1 is $sqr");
+
+  stdout.write("Enter no1");
+  int no1 = getIntValue();
+
+  stdout.write("Enter no2");
+  int no2 = getIntValue();
+
+  int sum = add(no1, no2);
+
+  print("The sum of $no1 and $no2 is $sum");
+
+}
+
+String getStrValue() => stdin.readLineSync().toString();
+
+int getIntValue() => int.parse(getStrValue());
+
+int add(int a, int b, [int c=0, int d=0]) => a+b+c+d;
