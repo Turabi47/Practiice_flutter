b74c0a45b5c752366d43473e5dd015a6362312b9
curious <akturabi47@gmail.com>
2026-02-26 12:34:17 +0500

    sfl

--- collections.dart
+++ collections.dart
@@ -1,7 +1,7 @@
 void main(){
   final List names1 = ["aziz", "khan", "turabi"];
   names1.add("Ahmed");
-  final List  names2 = ["ali","ahmed"];
+  final List  names2 = ["ali","ahmed", "khalid"];
   print(names1);
   // names1 = names2; cannot reassign in final
   // const is compile time constant but final is run time
